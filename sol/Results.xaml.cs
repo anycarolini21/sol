74598160d9776539d16925fb1479240d2f10c8c4
sol <anycarolinif340@gmail.com>
2024-04-16 11:51:38 -0300

    fazendo os frame

---
+++ sol/Results.xaml.cs
@@ -0,0 +1,22 @@
+using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+
+namespace sol;
+
+public class Results
+{
+public int temp {get; set;}
+public string descripton {get; set;}
+public string city {get; set;}
+public string currently {get; set;}
+public string date {get; set;} 
+public string codition_code {get; set;} 
+public string img_id {get; set;} 
+public string moon_phase {get; set;}
+public string date {get; set;}
+public string sunrise {get; set;}
+public string sunset {get; set;} 
+public string codition_code {get; set;}
+public double rain {get; set;} 
+public int humidity {get; set;} 
+
+}
