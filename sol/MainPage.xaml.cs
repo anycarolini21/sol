4a7df1166ffc2f715ab559cd90d105ef99364308
sol <anycarolinif340@gmail.com>
2024-04-24 11:48:44 -0300

    fazer chave

--- sol/MainPage.xaml.cs
+++ sol/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using Windows.ApplicationModel.AppService;
+﻿using System.Text.Json;
+using Windows.ApplicationModel.AppService;
 
 namespace sol;
 
 public partial class MainPage : ContentPage
 {
+
+  
 
 	public MainPage()
 	{
@@ -13,11 +16,43 @@ public partial class MainPage : ContentPage
 	Results = new Resposta
 }
 
+async Void AtualizaTempo()
+{
+	try
+	{
+		var httpClient= new HttpClient()
+		var Response = await HttpsClient.GetAsync(Url);
+		if (Response.IsSuccessStatusCode)
+		{
+			Var constant= await Response.Content.ReadAsStringAsync();
+			Resposta= JsonSerializer.Deserialize<Results>(content);
+		}
+		{
+			cactch (Exception e)
+			//Erro
+		}
+	}
+
+}
+
 void PreencherTela()
 {
 	Labeltemp.Text = Resposta.temp.ToString();
 	Labelcity.Text = Resposta.city;
 	Labelrain.Text = Resposta.rain.ToString():
+	LabelHumidity.Text = Resposta.results.Humidity.ToString();
+	LabelSunrise.Text = Resposta.results.Sunrise();
+	LabelSunset.Text =  Resposta.results.Sunset();
+	LabelWind_speedy.Text =  Resposta.results.wind_speedy();
+	LabelWind_direction.Text =  Resposta.results.Wind_direction();
+	 
+	if (AppServiceResponseStatus.ResourceLimitsExceeded.moon_phase=="full")
+	Labelmoon_phase.Text = "Cheia";
+	else if (resposta.results.moon_phase=="new")
+	Labelmoon_phase.Text = "Nova";
+
+
+
 	
 
 
